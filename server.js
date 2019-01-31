d93926cc606c41e17f0283db0e6c13df562fdd34
Aditya <adityavadityav@gmail.com>
2019-01-31 13:58:29 +0530

    Ready to deploy

--- server.js
+++ server.js
@@ -3,19 +3,15 @@ var app = express();
 
 app.use(express.static(__dirname+'/public'));
 app.set('view engine','hbs');
-app.use((req, res, next) => {
- var now = new Date().toString();
- console.log(now);
-//  next();
+// app.use((req, res, next) => {
+//  var now = new Date().toString();
+//  console.log(now);
+// //  next();
+// });
+
+app.get('/about',(req,res) => {
+    res.render('index.hbs');
 });
 
-// app.get('/',(req,res) => {
-//     res.render('index.hbs',{
-//         name:'Aditya',
-//     date: new Date().getFullYear()});
-// });
-// app.get('/home',(req,res) => {
-//     res.send("Hello home!");
-// });
 // console.log(new Date().getFullYear());
 app.listen('3000');
